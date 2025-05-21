1a0c05e24f6101a6e45ccea8d5124fe615f6783a
Julia <julocatelli66@gmail.com>
2025-05-20 23:27:03 -0300

    Segundo commit

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+import HomeScreen from './screens/HomeScreen';
+import DetailsScreen from './screens/DetailsScreen';
+import ProfileScreen from './screens/ProfileScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+    console.log(LoginScreen, HomeScreen, DetailsScreen, ProfileScreen);
+    return(
+        <NavigationContainer>
+            <Stack.Navigator initialRouteName = "Login">
+                <Stack.Screen name="Login" component = {LoginScreen} />
+                <Stack.Screen name = "Home" component = {HomeScreen} />
+                <Stack.Screen name = "Details" component = {DetailsScreen} />
+                <Stack.Screen name = "Profile" component = {ProfileScreen} />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+}
