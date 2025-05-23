90c12edfb571aeacd579456ad54e1206a79eb71a
Julia Locatelli <julocatelli66@gmail.com>
2025-05-22 21:22:42 -0300

    Adiciona tela de login

---
+++ Source/Screen/HomeScreen.js
@@ -0,0 +1,63 @@
+import React from 'react';
+import { View, Text, StyleSheet, Dimensions, TouchableOpacity } from 'react-native';
+
+const windowWidth = Dimensions.get('window').width;
+
+export default function HomeScreen({ navigation, route }) {
+  const { userEmail } = route.params || {};
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.email}>Bem-vindo: {userEmail}</Text>
+      <Text style={styles.title}>Home Screen</Text>
+
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => navigation.navigate('Details', { userEmail })}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>Go to Details</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={() => navigation.navigate('Profile', { userEmail })}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>Go to Profile</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a0937a',
+  },
+  email: {
+    position: 'absolute',
+    top: 60,
+    fontSize: 20,
+    color: '#2e2a23',
+  },
+  title: {
+    fontSize: 24,
+    marginBottom: 20,
+    color: '#2e2a23',
+  },
+  buttonContainer: {
+    backgroundColor: '#5f694f',
+    margin: 10,
+    width: windowWidth * 0.5,
+    borderRadius: 10,
+    paddingVertical: 12,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#000',
+    fontSize: 16,
+  },
+});
---
+++ Source/Screen/LoginScreen.js
@@ -0,0 +1,79 @@
+import React, { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Alert, TouchableOpacity } from 'react-native';
+
+export default function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [senha, setSenha] = useState('');
+
+  const handleLogin = () => {
+    if (email.trim() === '' || senha.trim() === '') {
+      Alert.alert('Erro', 'Por favor, preencha todos os campos.');
+    } else {
+      navigation.replace('Home', { userEmail: email }); // impede que o usuário volte à tela login
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Login</Text>
+
+      <TextInput
+        placeholder="Email"
+        value={email}
+        onChangeText={setEmail}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        style={styles.input}
+      />
+
+      <TextInput
+        placeholder="Senha"
+        value={senha}
+        onChangeText={setSenha}
+        secureTextEntry
+        style={styles.input}
+      />
+
+      <TouchableOpacity style={styles.button} onPress={handleLogin} activeOpacity={0.7}>
+        <Text style={styles.buttonText}>Entrar</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingHorizontal: 30,
+    backgroundColor: '#a0937a',
+  },
+  title: {
+    fontSize: 32,
+    marginBottom: 30,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    color: '#5f694f',
+  },
+  input: {
+    height: 50,
+    borderColor: '#2e2a23',
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    backgroundColor: '#a0937a',
+    color: '#5f694f ',
+  },
+  button: {
+    backgroundColor: '#758066',
+    paddingVertical: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#2e2a23',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
